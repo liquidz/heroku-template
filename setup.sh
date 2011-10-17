27aadba0ce729956eb7e96f85bd685507fabec49
Masashi Iizuka <liquidz.uo@gmail.com>
2011-10-17 23:49:34 +0900

    setup.shのaddonsディレクトリへの対応

--- setup.sh
+++ setup.sh
@@ -15,26 +15,24 @@ mv project.clj .backup
 sed -e "s/heroku-template/$NS/g" .backup/project.clj > project.clj
 
 # src
-#mv src .backup
 cp -pir src .backup
 git rm -r src
-mkdir -p "src/$NSPATH"
+mkdir -p "src/$NSPATH/addons"
 
 for FILE in `find .backup/src -type f`; do
-	NAME=`echo $FILE | cut -d"/" -f4`
+	NAME=`echo $FILE | cut -d"/" -f4-`
 	sed -e "s/heroku-template/$NS/g" $FILE > "src/$NSPATH/$NAME"
 done
 
 git add src
 
 # test
-#mv test .backup
 cp -pir test .backup
 git rm -r test
 mkdir -p "test/$NSPATH/test"
 
 for FILE in `find .backup/test -type f`; do
-	NAME=`echo $FILE | cut -d"/" -f5`
+	NAME=`echo $FILE | cut -d"/" -f5-`
 	sed -e "s/heroku-template/$NS/g" $FILE > "test/$NSPATH/test/$NAME"
 done
 
